2e1346ea02c49747cb1e659fd03d322646095121
Tim Malone (Mac) <tdmalone@gmail.com>
2017-05-30 08:46:34 +1000

    Minor documentation additions

--- _functions/pokemon/menus/onboarding-menu.php
+++ _functions/pokemon/menus/onboarding-menu.php
@@ -12,7 +12,7 @@ function slackemon_get_onboarding_menu() {
           ':pika2: *Yay! Welcome, new trainer!*' . "\n\n" .
           'Pokémon can appear at _any_ time of day or night - and you\'ll need to be quick to catch them! ' .
           'Don\'t worry though - you won\'t be bothered by Pokémon during your Slack \'do not disturb\' ' .
-          'hours, or while you have a :toggl: timer running.'
+          'hours.'
         ),
       ], [
         'title' => 'Ooh, what\'s that rustling in the bushes?!',
