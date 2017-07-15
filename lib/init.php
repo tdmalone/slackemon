90b7927fccc81ee37492ab336e18f4e78acb2ca4
Tim Malone <tdmalone@gmail.com>
2017-07-15 21:54:30 +1000

    Hotfix: improve production error logging & development error displaying

--- lib/init.php
+++ lib/init.php
@@ -7,9 +7,13 @@
 
 // Activate error logging.
 // TODO: This should be defined elsewhere rather than changing at runtime.
+ini_set( 'error_reporting', E_ALL );
+ini_set( 'log_errors', '1' );
+ini_set( 'display_errors', '0' );
+ini_set( 'display_startup_errors', '0' );
 if ( 'development' === getenv( 'APP_ENV' ) ) {
-  ini_set( 'log_errors', '1' );
-  ini_set( 'error_reporting', E_ALL );
+  ini_set( 'display_errors', '1' );
+  ini_set( 'display_startup_errors', '1' );
   ini_set( 'error_log', __DIR__ . '/../error_log' );
 }
 
